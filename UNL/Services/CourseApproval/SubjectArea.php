6ee4114d4c4d7e342d52d5b52fc2cb5d8c758287
Brett Bieber <brett.bieber@gmail.com>
2009-11-16 21:33:58 +0000

    Add support for retrieving course groups for each subject area.

--- UNL/Services/CourseApproval/SubjectArea.php
+++ UNL/Services/CourseApproval/SubjectArea.php
@@ -5,10 +5,18 @@ class UNL_Services_CourseApproval_SubjectArea
     
     public $courses;
     
+    /**
+     * array of groups if any
+     * @var UNL_Services_CourseApproval_SubjectArea_Groups
+     */
+    public $groups;
+    
     function __construct($subject)
     {
         $this->subject = $subject;
         $this->courses = new UNL_Services_CourseApproval_SubjectArea_Courses($this);
+        $groups = new UNL_Services_CourseApproval_SubjectArea_Groups($this);
+        $this->groups = $groups->groups;
     }
     
     function __toString()
